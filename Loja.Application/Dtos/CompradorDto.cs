9874725b6208f1422b8012572b672eeef6cbbf7b
Matheus Medeiros <matheusmedeiroswf@gmail.com>
2024-05-22 00:32:27 -0300

    Ajuste nas mensagens de erro aviso, ou toats da pagina Edit

--- Loja.Application/Dtos/CompradorDto.cs
+++ Loja.Application/Dtos/CompradorDto.cs
@@ -1,20 +1,34 @@
-﻿namespace Loja.Application.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Loja.Application.Dtos
 {
     public class CompradorDto
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O campo Nome/Razão Social é obrigatório.")]
         public string NomeRazaoSocial { get; set; }
+
+        [Required(ErrorMessage = "O campo E-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O campo E-mail não é um endereço de e-mail válido.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo Telefone é obrigatório.")]
         public string Telefone { get; set; }
         public DateTime DataCadastro { get; set; }
         public bool Bloqueado { get; set; }
         public string TipoPessoa { get; set; }
+
+        [Required(ErrorMessage = "O campo CPF e CPNJ é obrigatório.")]
         public string CpfCnpj { get; set; }
         public string? InscricaoEstadual { get; set; }
         public bool Isento { get; set; }
         public string? Genero { get; set; }
         public DateTime? DataNascimento { get; set; }
+
+        [StringLength(15, MinimumLength = 8, ErrorMessage = "A Senha deve ter entre 8 e 15 caracteres.")]
+        [Required(ErrorMessage = "O campo Senha é obrigatório.")]
         public string Senha { get; set; }
-        public string ConfirmacaoSenha { get; set; } // Adicionado
+
+        public string? ConfirmacaoSenha { get; set; }
     }
 }
